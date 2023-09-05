173b2f9b70a375d5ec83c9956d7aa0af4fdf82b9
DhansM <31100015+DhansM@users.noreply.github.com>
2023-09-05 12:36:53 +0530

    Update CarObjtest.cpp

--- test/CarObjtest.cpp
+++ test/CarObjtest.cpp
@@ -1,1 +1,29 @@
+class Vehicle{
+public:
+string model;
+string color;
+string name;
+int regno;
+};
+
+//parent child relation shop
+class car:public Vehicle{
+ Engine engObj;
+public:
+void Start() {
+  engObj.crankEngine();
+}
+};
+
+class Engine{
+private:
+FuelPump fuelpumpObj;
+StartupMotor motorObj;
+public:
+void crankEngine(){} //delegate-interact
+};
+
+class StartupMotor{};
+class FuelPump{};
+
 
