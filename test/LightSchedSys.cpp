6bbde55d18559e309468d18dab247c9752f4686d
DhansM <31100015+DhansM@users.noreply.github.com>
2023-09-05 12:04:50 +0530

    Update LightSchedSys.cpp

--- test/LightSchedSys.cpp
+++ test/LightSchedSys.cpp
@@ -1,1 +1,29 @@
+#include <string>
+using namespace std;
+class H3OHardware{};
+class RTOS{};
 
+class TimerService{
+private:
+RTOS rtosObj;
+public:
+string getTime(){return "";}
+};
+
+class LightController{
+private:
+H3OHardware hardwareObj;
+public:
+void on(){}
+void off(){}
+};
+
+class LightScheduler{
+private:
+TimerService timerObj;
+LightController controllerObj;
+public:
+void removeSchedule(){}
+void wakeup(){}
+void turnONschedule(){}
+};
